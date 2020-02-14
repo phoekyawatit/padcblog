320a3cd88edea98a74db80de9edf62a73a0ecb3e
thethlaing-mmdsgit config --global user.name thethlaing-mmds <thethlaing@mm-digital-solutions.com>
2020-02-15 05:45:56 +0630

    Baseline

---
+++ app/Post.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
+
+class Post extends Model
+{
+
+    use Searchable;
+    protected $fillable = ['title', 'content', 'is_published', 'author_id'];
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
+
+    public function getExcerptAttribute()
+    {
+        $exploded_string = explode(" ", $this->content);
+        return collect($exploded_string)->take(30)->join(' ') . ' ...';
+    }
+
+    public function scopePublished($query)
+    {
+        return $query->where('is_published', true);
+    }
+
+    public function scopePostOwner($query)
+    {
+        return $query->where('author_id', \Auth::user()->id);
+    }
+
+    public function scopeDefaultOrder($query)
+    {
+        return $query->orderByDesc('created_at');
+    }
+
+    // public function scopeSearch($query, $keyword)
+    // {
+    //     return $query->whereRaw('match(title,content) against (?)',$keyword);
+    //     // return $query->where('title', 'like', '%' . $keyword . '%')
+    //     //     ->orwhere('content', 'like', '%' . $keyword . '%');
+    // }
+}
---
+++ app/Services/PostService.php
@@ -0,0 +1,49 @@
+<?php
+namespace App\Services;
+
+use App\Mail\PostCreate;
+use App\Mail\PostUpdate;
+use App\Post;
+use \Cache;
+use \Mail;
+
+class PostService
+{
+
+    public function make($data)
+    {
+
+        $data['author_id'] = \Auth::user()->id;
+        $post = Post::create($data);
+
+        if(!$post->is_published){
+            $post->unsearchable();
+        }
+
+        Cache::forever('post.' . $post->id, $post);
+
+        Mail::to('th.ucsy@gmail.com')->send(
+            new PostCreate($post)
+        );
+
+        return $post;
+    }
+
+    public function update($data, $post)
+    {
+        $post->update($data);
+
+        if(!$post->is_published){
+            $post->unsearchable();
+        }
+
+        Cache::forever('post.' . $post->id, $post);
+
+        \Mail::to('th.ucsy@gmail.com')->send(
+            new PostUpdate($post)
+        );
+
+        return $post;
+    }
+
+}
